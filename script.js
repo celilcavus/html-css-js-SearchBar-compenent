641f445733a05ed81d35228421a256d24d6551d3
celilcavus <43173282+celilcavus@users.noreply.github.com>
2022-11-06 22:08:05 +0530

    Add files via upload
    basit bir search bar compenenti

---
+++ script.js
@@ -0,0 +1,10 @@
+const click = document.getElementById("searchBar");
+click = addEventListener("click",function(){
+  click.style.width = "100%";
+  click.style.boxShadow = "5px 1px 1px 1px gray"
+  click.style.transitionDuration = "2s";
+  click.style.textTransform = "capitalize";
+  click.style.borderBottom = "1px solid gray";
+})
+
+
